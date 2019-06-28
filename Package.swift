027653f4cccc0b3b8957c79b3433f94942cb3bf4
Andreas Link <andreas.link@jamitlabs.com>
2019-06-28 12:02:31 +0200

    Introduce Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "NVActivityIndicatorView",
+    products: [
+        .library(name: "NVActivityIndicatorView", targets: ["NVActivityIndicatorView"])
+    ],
+    targets: [
+        .target(
+            name: "NVActivityIndicatorView",
+            path: "Source"
+        )
+    ]
+)
